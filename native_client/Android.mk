76ca05fa476653c4124a1558f5f406f9daabc940
om3ga-solutions <75501637+om3ga-solutions@users.noreply.github.com>
2021-07-31 23:04:20 +0200

    0.9.7

---
+++ native_client/Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := stt-prebuilt
+LOCAL_SRC_FILES := $(TFDIR)/bazel-bin/native_client/libstt.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_CPP_EXTENSION    := .cc .cxx .cpp
+LOCAL_MODULE           := stt
+LOCAL_SRC_FILES        := client.cc
+LOCAL_SHARED_LIBRARIES := stt-prebuilt
+LOCAL_LDFLAGS          := -Wl,--no-as-needed
+include $(BUILD_EXECUTABLE)
